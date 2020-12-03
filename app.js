7866a7d91c6637d34fa4ed76101f248546b3ec35
Young Chi <young.chi@asetpartners.com>
2020-12-03 10:13:02 -0500

    added facilities routes

--- app.js
+++ app.js
@@ -4,11 +4,32 @@ const jwt = require('jsonwebtoken');
 // handles multipart/form-data for uploading files
 const multer  = require('multer');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const pool = require("./database");
 const router = require("./routes/router");
 
 const app = express();
+
+// Enable AllCORS Requests
+
+// const corsOptions = {
+//     origin: '127.0.0.1:5500',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+
+// app.use(cors())
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
--- routes/facilities.js
+++ routes/facilities.js
@@ -10,13 +10,27 @@ router.get(`/all`, (req, res, next) => {
         .catch(e=>next(e));
 });
 
+router.get(`/bysuborgid/:suborgId`, (req, res, next) => {
+    knex.select('*').from('facilities')
+    .where('suborgId', parseInt(req.params.suborgId))
+        .then(rows => {
+            res.status(200).send(rows);
+        })
+        .catch(e=>next(e));
+});
+
 router.post(`/add`, (req, res, next) => {
     console.log(req.body)
-    knex('facilities').insert([
-        {facilityName: req.body.facilityName},
-        {suborgId: req.body.suborgId},
-        {orgId: req.body.orgId}
-        ])
+    // knex('facilities').insert([
+    //     {facilityName: req.body.facilityName},
+    //     {suborgId: parseInt(req.body.suborgId)},
+    //     {orgId: parseInt(req.body.orgId)}
+    //     ])
+    knex('facilities').insert({
+        facilityName: req.body.facilityName,
+        suborgId: parseInt(req.body.suborgId),
+        orgId: parseInt(req.body.orgId)
+    })
     .then(result => {
         res.status(200).send(result)
     })
